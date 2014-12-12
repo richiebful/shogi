5c3d1a16d6c76556efe17fe66ad29ecb7a0b6d01
Richard Fulop <richardbfulop@gmail.com>
2014-12-11 19:41:16 -0500

    Adds genChildren "node"

--- src/include/ai.h
+++ src/include/ai.h
@@ -1,14 +1,9 @@
 /*Game tree node*/
-struct game_tn{
+struct tree_node{
   int potential;
   int raw_score;
   int score;
-  struct * game_tn;
-}
-
-/*Game tree row*/
-struct game_tr{
-  struct * game_tn nodes;
-  struct * game_tn best_tn;
-  double best_tn_score;
-}
+  int src[2], dst[2];
+  struct * tree_node parent;
+  struct * tree_row child_row;
+};
