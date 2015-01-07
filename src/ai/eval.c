002b63dd1d1ca86c045351435c976f18de7e58b5
Richard Fulop <richardbfulop@gmail.com>
2015-01-07 15:52:03 -0500

    Continue eval function

--- src/ai/eval.c
+++ src/ai/eval.c
@@ -1,9 +1,28 @@
 #include <shogi.h>
 #include <ai.h>
 
-int scoreNode(struct tree_node *node){
+int scoreNode(struct tree_node *node,
+	      int player){
   int score = 0;
+  int this_player = node->game.player;
   
+  int i, j;
+  int last_score, this_score;
+  FORRANGE(i, 0, 10, 1){
+    FORRANGE(j, 0, 10, 1){
+      
+    }
+  }
+
+  if (this_player == player){
+    score = this_score - last_score;
+  }
+  else{
+    score = last_score - this_score;
+  }
   return score;
 }
 
+int historyLength(){
+  
+}
