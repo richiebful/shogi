a65772581ac7cc57731b3a001264909a55d899bf
Richard Fulop <richardbfulop@gmail.com>
2015-01-10 19:45:19 -0500

    Adds to main text interface fn

--- src/engine/main.c
+++ src/engine/main.c
@@ -9,16 +9,24 @@ int main(void){
   while (fatal_f != true){
     char command[20] = "NULL";
     input_f = 0;
-    printf("Player %i:", game.player);
+    if (game.check_f == true){
+      printf("Check.\n");
+    }
+    printf("Player %i:\n", game.player);
     while (input_f == 0){
       scanf("%s", command);
       input_f = processcmd(command, &game);
     }
     if (input_f == 1){
       game.player = game.player % 2 + 1;
+      game.check_f = ischeck(game, game.player);
+      game.mate_f = ismate(game, game.player);
+      if (game.mate_f == true){
+	printf("Checkmate");
+	break;
+      }
     }
     if (input_f == -1){
-      //if move is fatal
       break;
     }
   }
