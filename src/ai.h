b245f4a31bb12897fa3cbbbbe46b88d5ba06f269
Richard Fulop <developerfulop@gmail.com>
2015-07-24 13:52:48 -0400

    Adds layer of abstraction within macro

--- src/ai.h
+++ src/ai.h
@@ -31,20 +31,24 @@ struct gm_ai_profile{
 
 struct pcValuation{
 	char name[NO_PIECES];
-	short score[NO_PIECES][MAIN_STAGES] = {{   0,  35,  70,  99 }, /* main stage borders */
-    {   7,   7,   8,  10 }, /* Pawn               */
-    {  20,  35,  45,  60 }, /* Lance              */
-    {  20,  35,  45,  60 }, /* Knight             */
-    {  35,  40,  60,  80 }, /* Silver             */
-    {  40,  55,  65,  80 }, /* Gold               */
-    {  90,  90,  90,  90 }, /* Bishop             */
-    {  95,  95,  95,  95 }, /* Rook               */
-    {  15,  25,  40,  65 }, /* promoted Pawn      */
-    {  25,  45,  55,  65 }, /* promoted Lance     */
-    {  25,  45,  55,  65 }, /* promoted Knight    */
-    {  35,  55,  75,  75 }, /* promoted Silver    */
-    {  99,  99,  99,  99 }, /* promoted Bishop    */
-    {  97,  97,  99,  99 }, /* promoted Rook      */
-    { 100, 100, 100, 100 }, /* King               */};
+	short score[NO_PIECES][MAIN_STAGES] = {{   0,  35,  70,  99}, /* main stage borders */
+					       {   7,   7,   8,  10 }, /* Pawn               */
+					       {  20,  35,  45,  60 }, /* Lance              */
+					       {  20,  35,  45,  60 }, /* Knight             */
+					       {  35,  40,  60,  80 }, /* Silver             */
+					       {  40,  55,  65,  80 }, /* Gold               */
+					       {  90,  90,  90,  90 }, /* Bishop             */
+					       {  95,  95,  95,  95 }, /* Rook               */
+					       {  15,  25,  40,  65 }, /* promoted Pawn      */
+					       {  25,  45,  55,  65 }, /* promoted Lance     */
+					       {  25,  45,  55,  65 }, /* promoted Knight    */
+					       {  35,  55,  75,  75 }, /* promoted Silver    */
+					       {  99,  99,  99,  99 }, /* promoted Bishop    */
+					       {  97,  97,  99,  99 }, /* promoted Rook      */
+					       { 100, 100, 100, 100 } /* King               */};
 }pcValue;
 
+/*
+ * pcValuation scores are better converted to continuous functions in order to circumvent boundary inaccuracies.
+ * For example, pawn_score = 10 - (3)*nthroot(round, 
+ */
