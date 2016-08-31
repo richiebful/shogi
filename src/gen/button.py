9c2b88fc0f704262ee0f923d68aba77f0dcfb67b
Richard Fulop <richardbfulop@gmail.com>
2016-08-30 20:11:12 -0400

    Adds stacks for tracking the board state as moves in the AI are executed Adds basic procedures to add stack elements Simplifies game tree nodes to only contain board/graveyard deltas

--- src/gen/button.py
+++
@@ -1,6 +0,0 @@
-for i in range(0, 9):
-    for j in range(0, 9):
-        #fix first line of console output
-        print("""button = gtk_button_new_with_label ("{0}");
-g_signal_connect (button, "clicked", G_CALLBACK (handler{0}{1}), NULL);
-gtk_grid_attach (GTK_GRID (grid), button, {1}, {0}, 1, 1);\n""".format(i, j))
